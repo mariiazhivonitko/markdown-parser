492b29202d48059616e40c7aea00fe5ecd0a908c
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2025-02-05 13:09:27 +0200

    first commit

---
+++ markdown_parser_test.py
@@ -0,0 +1,36 @@
+import re
+
+# The text containing the table
+text = """
+Here are the proposed headers, possible units, and data types for the columns:
+
+| Header | Possible Units | Data Type |
+| --- | --- | --- |
+| Date | YYYY-MM-DD, DD-MM-YYYY, MM-DD-YY | date |
+| Time | HH:MM:SS, HHMMSS | time |
+| Distance | meters, kilometers, miles | float |
+| Speed | km/h, mph, m/s | float |
+| Pace | minutes per kilometer, minutes per mile, seconds per meter | float |
+| Cadence | steps per minute, revolutions per minute | float |
+| Power | watts | float |
+| Heart Rate | beats per minute | int |
+| Activity Type | walking, running, cycling | string |
+| User Name | text | string |
+
+Please note that the above headers are just proposed and may need to be adjusted based on the specific requirements of your data analysis.
+"""
+
+#def extract_markdown_table(text):
+#   """Extracts the data from a markdown table.
+
+#   Args:
+#     markdown_table: A string containing the markdown table.
+
+#   Returns:
+#     Dataframe.
+#   """
+
+match = re.search(r"(\|.*?\|\n(\|[-: ]+\|\n)?(\|.*?\|\n?)+)", text, re.DOTALL)
+table_text = match.group(1)
+print(table_text)
+
