728990eca21cda7e90a5ca6f8a10d180f8cb925e
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2025-02-06 09:30:28 +0200

    conversion to dataframe

--- markdown_parser_test.py
+++ markdown_parser_test.py
@@ -26,10 +26,13 @@ table_text = match.group(1)
 
 table_lines = table_text.strip().split("\n")
 
-cleaned_lines = [line.strip("|") for line in table_lines]
+# Remove markdown separator row (--- | --- | ---)
+table_lines = [line for line in table_lines if "---" not in line]
 
+cleaned_lines = [line.strip("|").split("|") for line in table_lines]
+cleaned_cells = [list(map(str.strip, line)) for line in cleaned_lines]
 
+df = pd.DataFrame(cleaned_cells[1:], columns = cleaned_cells[0])
 
-
-print(cleaned_lines)
+print(df)
 
